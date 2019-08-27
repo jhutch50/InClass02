eed60fcdfaeea98671ada460aabbb624c12e242d
Joshua Hutcheson <jhutch50@uncc.edu>
2019-08-26 21:29:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/activity2/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.activity2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_shape;
+    private TextView result;
+    private Button button_calculate;
+    private Button button_clear;
+    private ImageView triangle;
+    private ImageView square;
+    private ImageView circle;
+    private EditText et_length1;
+    private EditText et_length2;
+    private String selectedShape;
+    Float length1 = 0.0f;
+    Float length2 = 0.0f;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Area Calculator");
+
+        et_length1 = findViewById(R.id.editLength1);
+        et_length2 = findViewById(R.id.editLength2);
+        result = findViewById(R.id.tv_result);
+        tv_shape = findViewById(R.id.tv_shape);
+        button_calculate = findViewById(R.id.button_calculate);
+        button_clear = findViewById(R.id.button_clear);
+        triangle = findViewById(R.id.triangle);
+        square = findViewById(R.id.square);
+        circle = findViewById(R.id.circle);
+        clearButton();
+
+
+        String tempLength1 = et_length1.getText().toString();
+        String tempLength2 = et_length2.getText().toString();
+        if(tempLength1!=null && !tempLength1.equals(""))
+            length1 = Float.parseFloat(et_length1.getText().toString());
+        if(tempLength2!=null && !tempLength2.equals(""))
+            length2 = Float.parseFloat(et_length2.getText().toString());
+
+        button_clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                clearButton();
+            }
+        });
+
+        triangle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                selectedShape = "triangle";
+                tv_shape.setText("Triangle");
+                et_length1.setVisibility(View.VISIBLE);
+                et_length2.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+//          TODO: On Click for Square.....
+//          TODO: On Click for Cicrcle.....
+
+        button_calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if(et_length1.getText().toString().equals("")){
+                    et_length1.setError("Hey, I need a value!");
+                }
+                if(et_length2.getText().toString().equals("")){
+                    et_length2.setError("Hey, I need a value!");
+                }
+                switch(selectedShape) {
+                    case "triangle":
+                        float result = 0.5f * length1 * length2;
+                }
+            }
+        });
+
+
+    }
+
+    public void clearButton(){
+        et_length1.setVisibility(View.VISIBLE);
+        et_length2.setVisibility(View.VISIBLE);
+        et_length1.setText("");
+        et_length2.setText("");
+//        tv_shape.setText("Select a shape");
+//        triangle.setVisibility(View.VISIBLE);
+//        circle.setVisibility(View.VISIBLE);
+
+    }
+}
